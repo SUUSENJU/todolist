41dfa4bac80fc000b9197cb850b9973e8ca987f9
SUUSENJU <moura040222009@gmail.com>
2023-04-25 09:53:53 -0300

    a

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const projectName = 'front-end'
 
-app.use(express.static(__dirname) + '/dist');
+app.use(express.static(__dirname + '/dist/' + projectName));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + 'dist/front-end/index.html');
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/dist/' + projectName + '/index.html');
 });
 
 app.listen(4200);
