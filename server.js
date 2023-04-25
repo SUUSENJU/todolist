3cc1dc9b6c861ff88a8a735b222a86c55eb2224b
SUUSENJU <moura040222009@gmail.com>
2023-04-25 09:49:59 -0300

    reconfig server.js

--- server.js
+++ server.js
@@ -1,37 +1,8 @@
-// const express = require('express')
-// const { resolve } = require('path')
-// const app = express()
-// const path = require('path');
+const express = require('express');
+const app = express();
 
-// app.use('/',
-//   express.static(
-//     resolve(
-//       __dirname,
-//       './build'
-//     )
-//   )
-// )
+app.use(express.static(__dirname) + '/dist');
 
-// app
-//   .listen(process.env.PORT || 3000, (err) => {
-//     if (err) { return console.log(err) }
-
-//     console.log('Tudo funcionando certinho')
-//   })
-
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-const express = require('express')
-const path = require('path')
-
-const PORT = process.env.PORT || 5001
-
-express()
-  .use(express.static(path.join(__dirname, './build')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))})
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + 'dist/front-end/index.html');
+});
